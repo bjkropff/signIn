be554204dcaf285d9c44b52ee802cfe9c7234367
Brian Kropff <brian.kropff@gmail.com>
2015-04-24 13:42:09 -0700

    Replace links with buttons on the sign in page

--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,6 +1,13 @@
 studentLog.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
-  factory.students = [{ name: "Jane Doe", inClass: false }, { name: "John Doe", inClass: true }]
+  factory.students = [
+    { name: "Jane Doe", inClass: false },
+    { name: "John Doe", inClass: true },
+    { name: "Wedge Antilles", inClass: false },
+    { name: "Biggs Darklighter", inClass: true },
+    { name: "Lando Calrissian", inClass: false },
+    { name: "Chewbacca", inClass: true }
+    ]
   factory.addStudent = function() {
     factory.students.push({ name: factory.studentName, inClass: false})
     factory.studentName = null;
